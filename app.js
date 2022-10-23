72dc4de48e1ca5ca8ac4adb9bbcc780984f75687
Vytautas Sv <vytautassvirskas@gmal.com>
2022-10-23 19:46:59 +0300

    show error if inputs are empty

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const data = {
     name: document.querySelector("#cardholder-name"),
     number: document.querySelector("#card-number"),
@@ -5,7 +7,6 @@ const data = {
     expireDateYear: document.querySelector("#card-expire-year"),
     cvc: document.querySelector("#card-cvc")
 }
-
 
 const card ={
     name: document.querySelector(".front-card__fullname"),
@@ -15,8 +16,17 @@ const card ={
     cvc: document.querySelector(".back-card__cvc")
 }
 
+const confirm = document.querySelector(".form__btn");
+const form = document.querySelector(".form");
+
 const fillData = (inputName, cardPlace) => {
     inputName.addEventListener("input", (e) => {
+        if(e.target.value === " ") return
+        if(inputName== data.number) {
+            // e.target.value = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
+            e.target.value = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
+
+        }
         cardPlace.innerText = e.target.value;
     })
 }
@@ -26,14 +36,35 @@ fillData(data.expireDateMonth, card. expireDateMonth); //max 12 menesiu, tai 12 
 fillData(data.expireDateYear, card.expireDateYear); //max 99 metu, tai 99 maks
 fillData(data.cvc, card.cvc);
 
-// data.expireDateMonth.addEventListener("input", (e) => {
-//     card.name.innerText = e.target.value;
-// })
-
-
 // data.name.addEventListener("input", (e) => {
 //     console.log("e.target.value: ", e.target.value);
 //     card.name.innerText = e.target.value;
 // })
-console.log(data.name)
-console.log(card)
+
+confirm.addEventListener("click", (e) => {
+    e.preventDefault();
+    data.name.value === "" 
+    ? 
+    data.name.nextElementSibling.classList.remove("form__error-message--hiden")
+    :
+    data.name.nextElementSibling.classList.add("form__error-message--hiden")
+
+    data.number.value === "" ?
+    data.number.nextElementSibling.classList.remove("form__error-message--hiden")
+    :
+    data.number.nextElementSibling.classList.add("form__error-message--hiden")
+
+    if(data.expireDateMonth.value === "" || data.expireDateYear.value==="") {
+        data.expireDateYear.parentElement.nextElementSibling.classList.remove("form__error-message--hiden");
+    }else {
+        data.expireDateYear.parentElement.nextElementSibling.classList.add("form__error-message--hiden");
+    }
+    data.cvc.value === "" 
+    ?
+    data.cvc.nextElementSibling.classList.remove("form__error-message--hiden") 
+    :
+    data.cvc.nextElementSibling.classList.add("form__error-message--hiden")
+
+    // form.classList.add("form--hidden");
+
+})
