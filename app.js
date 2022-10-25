089b2e852f59c9da295bb3d5676ebfbd7987caef
Vytautas Sv <vytautassvirskas@gmal.com>
2022-10-25 23:00:57 +0300

    updated input validation after confirm

--- app.js
+++ app.js
@@ -30,58 +30,84 @@ const fillData = (inputName, cardPlace) => {
     inputName.addEventListener("input", (e) => {
         if(e.target.value === " ") return
         if(inputName== data.number) {
-            // e.target.value = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
-            e.target.value = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
+            e.target.value = e.target.value.replace(/[^\dA-Z]/gi, '').replace(/(.{4})/g, '$1 ').trim();
         }
         cardPlace.innerText = e.target.value;
     })
 }
 fillData(data.name, card.name);
-fillData(data.number, card.number); //prideti tarpus po 4 simboliu
-fillData(data.expireDateMonth, card. expireDateMonth); //max 12 menesiu, tai 12 maks
-fillData(data.expireDateYear, card.expireDateYear); //max 99 metu, tai 99 maks
+fillData(data.number, card.number);
+fillData(data.expireDateMonth, card. expireDateMonth);
+fillData(data.expireDateYear, card.expireDateYear);
 fillData(data.cvc, card.cvc);
+
+const messageError = (messagePLace, messageText, inputName1, inputName2 ) => {
+    messagePLace.innerText = messageText;
+    messagePLace.classList.remove("form__error--hiden")
+    inputName1 && inputName1.classList.add("form__input--error")
+    inputName2 && inputName2.classList.add("form__input--error")
+
+}
+
+const deleteMessageError = (messagePLace, inputName1, inputName2 ) => {
+    messagePLace.classList.add("form__error--hiden")
+    inputName1.classList.remove("form__input--error")
+    inputName2 && inputName2.classList.remove("form__input--error")
+}
 
 confirm.addEventListener("click", (e) => {
     e.preventDefault();
     const numbers = /^[0-9]+$/;
+
     // name
     if(data.name.value === ""){
-        message.name.classList.remove("form__error--hiden")
+        messageError(message.name, "Cant't be blank",data.name )
     }else{
-        message.name.classList.add("form__error--hiden")
+        deleteMessageError(message.name, data.name)
     }
+    
     // card number
     if(data.number.value === ""){
-        message.number.classList.remove("form__error--hiden")
-    }else if(data.number.value.match(numbers) && data.number.value.length === 6){
-        console.log("yra suvesti skaiciai card number")
-        message.number.innerText="Cant't be blank"
-        message.number.classList.add("form__error--hiden")
+        messageError(message.number, "Cant't be blank",data.number )
+    }else if(+data.number.value.replace(/\s/g, '').match(numbers) && data.number.value.replace(/\s/g, '').length === 16){
+        deleteMessageError(message.number, data.number)
+    }else if(+data.number.value.replace(/\s/g, '').match(numbers) && data.number.value.replace(/\s/g, '').length !== 16){
+        messageError(message.number, "Card number must be 16 digits")
     }
     else{
-        console.log("yra kitu simboliu card number")
-        message.number.innerText="Wrong format, numbers only"
-        message.number.classList.remove("form__error--hiden")
+        messageError(message.number, "Wrong format, numbers only")
     }
-    // cvc
-    if(data.cvc.value === "" ){
-        message.cvc.classList.remove("form__error--hiden") 
-    }else if(data.cvc.value.match(numbers) && data.cvc.value.length === 3){
-        console.log("yra suvesti skaiciai CVC")
-        message.cvc.innerText="Cant't be blank"
-        message.cvc.classList.add("form__error--hiden")
 
-    }else{
-        console.log("yra kitu simboliu CVC")
-        message.cvc.innerText="Wrong format, numbers only"
-        message.cvc.classList.remove("form__error--hiden")
-    }
     // expire date
     if(data.expireDateMonth.value === "" || data.expireDateYear.value==="") {
-        message.expireDate.classList.remove("form__error--hiden");
-    }else {
-        message.expireDate.classList.add("form__error--hiden");
-    }
+        messageError(message.expireDate, "Cant't be blank",data.expireDateMonth, data.expireDateYear )
+    }else if(
+        data.expireDateMonth.value.match(numbers) && data.expireDateMonth.value >0 && data.expireDateMonth.value <=12
+        && data.expireDateYear.value.match(numbers) && data.expireDateYear.value >0 && data.expireDateYear.value <=99
+        ){
+        deleteMessageError(message.expireDate, data.expireDateMonth, data.expireDateYear)
+    }else if(
+        (data.expireDateMonth.value.match(numbers) && (data.expireDateMonth.value <=0 || data.expireDateMonth.value >12))
+        || 
+        (data.expireDateYear.value.match(numbers) && (data.expireDateYear.value <22 || data.expireDateYear.value >99))
+        ){
+            messageError(message.expireDate, "MM must be between 1-12 and YY between 01-99",data.expireDateMonth, data.expireDateYear )
+        }
+        else {
+            messageError(message.expireDate, "Wrong format, numbers only",data.expireDateMonth, data.expireDateYear )
+        }
+        
+        // cvc
+        if(data.cvc.value === "" ){
+            messageError(message.cvc, "Cant't be blank",data.cvc )
+        }
+        else if(data.cvc.value.match(numbers) && data.cvc.value.length !== 3){
+            messageError(message.cvc, "CVC must be 3 digits")
+        }
+        else if(data.cvc.value.match(numbers) && data.cvc.value.length === 3){
+            deleteMessageError(message.cvc, data.cvc)
+        }else{
+            messageError(message.cvc, "Wrong format, numbers only")
+        }
     
 })
